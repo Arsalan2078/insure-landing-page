97993325552c04b20a8452c164d9de4372fd00da
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-07-07 22:40:40 -0400

    starter files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem laudantium
+      atque dignissimos nostrum dolorem, earum dolores alias deserunt, eaque
+      adipisci voluptatum neque commodi non corrupti dolore minus iure! Ex,
+      harum.
+    </>
+  );
+}
+
+export default App;
