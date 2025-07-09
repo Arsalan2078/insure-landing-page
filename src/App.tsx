02d74ffdf4fe219bf62c0b51df9a6e4a219d6205
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-07-08 23:52:04 -0400

    added button styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import classes from "./App.module.scss";
 function App() {
   return (
     <>
+      <a href="#" className="button button--primary">
+        View Plans
+      </a>
+
+      <a href="#" className="button button--secondary">
+        View Plans
+      </a>
+
       <div className={classes.app}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
         laudantium atque dignissimos nostrum dolorem, earum dolores alias
